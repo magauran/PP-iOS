42a5fcd25c079c072dcba2b10c251e90f7da4fd6
Alexey Salangin <a7ls@ya.ru>
2018-03-04 07:12:22 +0300

    Add Recipe and Ingredient models

---
+++ PP/Models/Ingredient.swift
@@ -0,0 +1,16 @@
+//
+//  Ingredient.swift
+//  PP
+//
+//  Created by Алексей on 04.03.2018.
+//  Copyright © 2018 Алексей. All rights reserved.
+//
+
+import Foundation
+
+class Ingredient {
+    
+    var value: String!
+    var name: String!
+    
+}
---
+++ PP/Models/Recipe.swift
@@ -0,0 +1,20 @@
+//
+//  Recipe.swift
+//  PP
+//
+//  Created by Алексей on 04.03.2018.
+//  Copyright © 2018 Алексей. All rights reserved.
+//
+
+import Foundation
+
+class Recipe {
+    
+    var ingredients: [Ingredient]!
+    var photo: String!
+    var title: String!
+    var time: String!
+    var category: Int!
+    var recipe: [String]!
+    
+}
